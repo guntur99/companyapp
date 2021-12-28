a3933e9b67385c084e9f526f8008f0a816c49e80
guntur99 <gugunguntur99@gmail.com>
2021-12-28 16:00:53 +0700

    fix typo

--- resources/views/layouts/dashboard/menus/employees/list.blade.php
+++ resources/views/layouts/dashboard/menus/employees/list.blade.php
@@ -13,7 +13,7 @@
                 <div class="row">
                     <div class="col-12 text-white p-t-40 p-b-90">
                         <h4 class="">
-                           Company List
+                           Employee List
                         </h4>
                     </div>
                 </div>
